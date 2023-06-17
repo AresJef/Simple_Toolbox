7e44b381a74c574f268e3d6432c411cdba317a1e
MacbookPro-14 <keppa1991@163.com>
2023-06-17 20:19:00 +0800

    bugfix on installation error

--- setup.py
+++ setup.py
@@ -5,26 +5,30 @@ from setuptools import setup, find_packages
 
 
 def build_extension(cython_dir: str) -> list[Extension]:
-    dir = os.path.abspath(os.path.dirname(__file__)) + cython_dir
+    dir = os.path.abspath(os.path.dirname(__file__)) + "/src/" + cython_dir
     pyx_list = [i for i in os.listdir(dir) if i.endswith(".pyx")]
+    pyx_src = [os.path.join(dir, i) for i in pyx_list]
+    pyx_name = [
+        cython_dir.replace("/", ".") + "." + i[:-4].replace("/", ".") for i in pyx_list
+    ]
     return [
         Extension(
-            name=pyx.replace(".pyx", ""),
-            sources=[os.path.join(dir, pyx)],
+            name=name,
+            sources=[src],
             extra_compile_args=[
                 "-Wno-unreachable-code-fallthrough",
                 "-Wno-unused-function",
             ]
-            if pyx == "dt_parser_c.pyx"
+            if src.endswith("dt_parser_c.pyx")
             else [],
         )
-        for pyx in pyx_list
+        for name, src in zip(pyx_name, pyx_src)
     ]
 
 
 setup(
     ext_modules=cythonize(
-        build_extension("/src/simple_toolbox/cython_core"),
+        build_extension("simple_toolbox/cython_core"),
         compiler_directives={"language_level": "3"},
     ),
 )
